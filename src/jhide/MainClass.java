4dd04952633ccc3120c4918ff1b710f2940e085f
Mongogamer <Mongogamer@users.noreply.github.com>
2017-03-11 23:08:32 +0100

    Case insensitive parameters
    * Version 1.0.1
    Changed all options to be case insensitive

--- src/jhide/MainClass.java
+++ src/jhide/MainClass.java
@@ -5,10 +5,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainClass {
-	private static String version = "1.0.0";					//Global version identifier
+	private static String version = "1.0.1";					//Global version identifier
 	
 	
 	
@@ -26,15 +27,15 @@ public class MainClass {
 		System.out.println("     _ _   _ _     _      \n    | | | | (_) __| | ___ \n _  | | |_| | |/ _` |/ _ \\\n| |_| |  _  | | (_| |  __/\n \\___/|_| |_|_|\\__,_|\\___|");
 		System.out.println("By Mongo_gamer\n");
 		
-		if(args.length == 0 || Arrays.asList(args).contains("-h") || Arrays.asList(args).contains("--help")){
+		if(args.length == 0 || containsCaseInsensitive("-h", Arrays.asList(args)) || containsCaseInsensitive("--help", Arrays.asList(args))){
 			printhelp();
 		}
 		
-		if(Arrays.asList(args).contains("-v") || Arrays.asList(args).contains("--version")){
+		if(containsCaseInsensitive("-v", Arrays.asList(args)) || containsCaseInsensitive("--version", Arrays.asList(args))){
 			System.out.println(version);
 			System.exit(0);
 		}
-		if(Arrays.asList(args).contains("--supportedformats")){
+		if(containsCaseInsensitive("--supportedformats", Arrays.asList(args))){
 			System.out.println("Supported archives: " + archiveext + "\n" + "Supported images: " + imageext);
 			System.exit(0);
 		}
@@ -42,7 +43,7 @@ public class MainClass {
 		String switchstring;
 		for(int i = 0; i < args.length - 1; i++){
 			switchstring = args[i].toString();
-			switch(switchstring){
+			switch(switchstring.toLowerCase()){
 			
 			case "-ii":
 				image = new File(args[i+1]);
@@ -87,7 +88,7 @@ public class MainClass {
 					 System.exit(1);
 				 }
 				if(output.getAbsoluteFile().exists()){
-					Scanner reader = new Scanner(System.in);  // Reading from System.in
+					Scanner reader = new Scanner(System.in);	//Reading from System.in
 					System.out.println("A file with the same name already exists. Do you wish to overwrite it? (Y/N)");
 					String input = reader.nextLine();
 					if(input.equalsIgnoreCase("y")){
@@ -108,7 +109,7 @@ public class MainClass {
 					 System.exit(1);
 				 }
 				if(output.getAbsoluteFile().exists()){
-					Scanner reader = new Scanner(System.in);  // Reading from System.in
+					Scanner reader = new Scanner(System.in);	// Reading from System.in
 					System.out.println("A file with the same name already exists. Do you wish to overwrite it? (Y/N)");
 					String input = reader.nextLine();
 					if(input.equalsIgnoreCase("y")){
@@ -161,5 +162,14 @@ public class MainClass {
 					+	"JHide -ii \"path/to/image.*\" --inputarchive \"path/to/archive.*\" -o \"path/to/output.*\"");
 			System.exit(0);
 		}
+		
+		public static boolean containsCaseInsensitive(String s, List<String> l){
+		     for (String string : l){
+		        if (string.equalsIgnoreCase(s)){
+		            return true;
+		         }
+		     }
+		    return false;
+		  }
 	
 	}
